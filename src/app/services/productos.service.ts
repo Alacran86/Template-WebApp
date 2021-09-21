01b59606b0a7953ec1de6922761ecb3dd9fa230a
Alacran86 <alacran.mlr@gmail.com>
2021-09-21 12:18:45 -0300

    search

--- src/app/services/productos.service.ts
+++ src/app/services/productos.service.ts
@@ -9,25 +9,69 @@ export class ProductosService {
 
   cargando = true;
   productos: Producto[] = [];
+  productosFiltrado: Producto[] = [];
+
   constructor( private http: HttpClient) { 
     this.cargarProductos();
   }
 
   private cargarProductos() {
-   
-    this.http.get('https://template-webapp-default-rtdb.firebaseio.com/productos_idx.json')
-    .subscribe( (resp: Producto[]) => {
 
-    this.productos = resp;
-    this.cargando = false;
+    return new Promise((resolve, reject) => {
 
-    
+      this.http.get('https://template-webapp-default-rtdb.firebaseio.com/productos_idx.json')
+      .subscribe( (resp: Producto[]) => {
+  
+      this.productos = resp;
+      this.cargando = false;
+      resolve();
+      });
 
     });
+   
+
+  
   }
   
   getProductos(id: string) {
     return this.http.get(`https://template-webapp-default-rtdb.firebaseio.com/productos/${ id }.json`);
+
+  }
+
+  buscarProducto( termino: string ){
+
+    if (this.productos.length === 0){
+      
+      this.cargarProductos().then( () => {
+     
+      this.filtrarProductos( termino );
+      });
+
+    } else {
+      this.filtrarProductos( termino );
+    }
+
+
+    
+
+  }
+
+  private filtrarProductos( termino: string) {
+  
+    console.log(this.productos);
+    this.productosFiltrado = []; 
+
+    termino = termino.toLowerCase();
+
+    this.productos.forEach( prod => {
+
+      const tituloLower = prod.titulo.toLowerCase();
+
+      
+    if (prod.categoria.indexOf( termino ) >= 0 || tituloLower.indexOf( termino ) >= 0 ) {
+      this.productosFiltrado.push( prod );
+    }
+    })
 
   }
 
